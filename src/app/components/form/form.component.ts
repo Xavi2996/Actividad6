f660856f765d0b748ee7abe4c858c85dd8917e70
xavier <xavier.cobos.29@hotmail.com>
2023-08-15 13:05:13 -0500

    Actualización y creación, fuentes, animaciones popups

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { UsersService } from 'src/app/services/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form',
@@ -18,10 +19,15 @@ export class FormComponent {
 
   constructor() {
     this.userForm = new FormGroup({
-      name: new FormControl('', []),
-      lastname: new FormControl('', []),
-      email: new FormControl('', []),
-      image: new FormControl('', []),
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      lastname: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(
+          /^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/
+        ),
+      ]),
+      image: new FormControl('', [Validators.required]),
     });
   }
 
@@ -34,10 +40,18 @@ export class FormComponent {
         this.arrUser = this.usersService.getUserById(id).subscribe((data) => {
           this.userForm = new FormGroup({
             id: new FormControl(data._id, []),
-            name: new FormControl(data.first_name, []),
-            lastname: new FormControl(data.last_name, []),
-            email: new FormControl(data.email, []),
-            image: new FormControl(data.image, []),
+            name: new FormControl(data.first_name, [
+              Validators.required,
+              Validators.minLength(3),
+            ]),
+            lastname: new FormControl(data.last_name, [Validators.required]),
+            email: new FormControl(data.email, [
+              Validators.required,
+              Validators.pattern(
+                /^((([!#$%&'*+\-/=?^_`{|}~\w])|([!#$%&'*+\-/=?^_`{|}~\w][!#$%&'*+\-/=?^_`{|}~\.\w]{0,}[!#$%&'*+\-/=?^_`{|}~\w]))[@]\w+([-.]\w+)*\.\w+([-.]\w+)*)$/
+              ),
+            ]),
+            image: new FormControl(data.image, [Validators.required]),
           });
         });
       }
@@ -49,11 +63,40 @@ export class FormComponent {
     if (this.userForm.value.id) {
       this.usersService.update(this.userForm.value).subscribe((data) => {
         console.log(data);
+        Swal.fire({
+          title: `Usuario ${data.first_name} ha sido actualizado`,
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown',
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp',
+          },
+        });
       });
     } else {
       this.usersService.createUser(this.userForm.value).subscribe((data) => {
         console.log(data);
+        Swal.fire({
+          title: `Usuario ${this.userForm.value.name} ha sido creado`,
+          showClass: {
+            popup: 'animate__animated animate__fadeInDown',
+          },
+          hideClass: {
+            popup: 'animate__animated animate__fadeOutUp',
+          },
+        });
       });
     }
+  }
+
+  errorControl(
+    formControlName: string,
+    validator: string
+  ): boolean | undefined {
+    //para no escribir tanto código en el html para labels de error de cada input
+    return (
+      this.userForm.get(formControlName)?.hasError(validator) &&
+      this.userForm.get(formControlName)?.touched
+    );
   }
 }
--- src/app/pages/view-user/view-user.component.ts
+++ src/app/pages/view-user/view-user.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { UsersService } from 'src/app/services/users.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-view-user',
@@ -14,14 +15,63 @@ export class ViewUserComponent {
   usersService = inject(UsersService);
   router = inject(Router);
   oneUser!: User | any;
+  id: string = '';
 
   ngOnInit(): void {
     //Capturar Ruta Activa
     this.activateRoute.params.subscribe((params) => {
-      let id = params['id'];
-      this.oneUser = this.usersService.getUserById(id).subscribe((data) => {
-        this.oneUser = data;
-      });
+      this.id = params['id'];
+      this.oneUser = this.usersService
+        .getUserById(this.id)
+        .subscribe((data) => {
+          this.oneUser = data;
+        });
     });
+  }
+
+  deleteUser() {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: 'btn btn-success mx-3',
+        cancelButton: 'btn btn-danger',
+      },
+      buttonsStyling: false,
+    });
+
+    swalWithBootstrapButtons
+      .fire({
+        title: `Deseas eliminar al usuario ${this.oneUser.first_name}`,
+        text: 'Para confirmar presione en aceptar',
+        showCancelButton: true,
+        allowOutsideClick: false,
+        showCloseButton: true,
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar',
+        reverseButtons: false,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.usersService.deleteUser(this.id).subscribe((data) => {
+            if (data) {
+              swalWithBootstrapButtons.fire(
+                'Usuario Eliminado!',
+                ` ${this.oneUser.first_name} borrado correctamente.`,
+                'success'
+              );
+            }
+            this.router.navigate(['/home']);
+          });
+        } else if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.cancel
+        ) {
+          swalWithBootstrapButtons.fire(
+            'Acción cancelada',
+            `${this.oneUser.first_name} no a sido borrada.`,
+            'error'
+          );
+        }
+        this.router.navigate(['/home']);
+      });
   }
 }
